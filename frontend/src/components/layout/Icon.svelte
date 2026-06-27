6cac4790e1752f16291468316dac794228174e98
Philip Reese <philipreese@mail.com>
2026-06-27 12:19:15 -0400

    chore(frontend): Fix prettier formatting on art-related components

--- frontend/src/components/layout/Icon.svelte
+++ frontend/src/components/layout/Icon.svelte
@@ -33,7 +33,9 @@
     class="{size} {className}"
     {style}
   >
-    <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z" />
+    <path
+      d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"
+    />
   </svg>
 {:else if name === 'logo'}
   <!-- Record logo: renders its own SVG because inner circles need mixed fill colors -->
